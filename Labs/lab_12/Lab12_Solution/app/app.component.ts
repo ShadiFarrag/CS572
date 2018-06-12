6120c683fe20edce4de7ab19c69a86f25cc272e6
ShadiFarrag <sfarrag@mum.edu>
2018-06-12 14:47:00 -0500

    This is Lab 12 Solution !!

---
+++ Labs/lab_12/Lab12_Solution/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Lab 12';
+  counter = 0;
+  componentCounterValue = 2009;
+
+  counterUpdated(event){
+    this.counter = event;
+    this.componentCounterValue = event;
+  }
+
+}
---
+++ Labs/lab_12/Lab12_Solution/app/counter.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input, EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+    <p>
+      <button (click)="decrease()">-</button>
+      {{counterValue}}
+      <button (click)="increase()">+</button>      
+    </p>
+  `,
+  styles: []
+})
+export class CounterComponent implements OnInit {
+  @Input("counterValue") counterValue: number;
+  @Output() counterEmitter: EventEmitter<string>;
+  @Input() componentCounterValue: number;
+
+  constructor() { 
+    // this.counterValue = 0;
+    this.counterEmitter = new EventEmitter();
+  }
+
+  ngOnInit() {
+  }
+
+  increase(){
+    this.counterValue ++;
+    this.counterEmitter.emit(this.counterValue+"");
+    return false;
+  }
+
+  decrease(){
+    this.counterValue --;
+    this.counterEmitter.emit(this.counterValue+"");
+    return false;
+  }
+
+}
